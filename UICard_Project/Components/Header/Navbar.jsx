34b3b401c88ed8e563e3543fe4d6f7b26afb73db
AliFaryadDev <muhammadalifaryad.social@gmail.com>
2026-09-21 12:00:26 +0500

    Add files via upload

--- UICard_Project/Components/Header/Navbar.jsx
+++ UICard_Project/Components/Header/Navbar.jsx
@@ -1,1 +1,13 @@
+function NAV (props){
+  return(
+    <>
+    <nav>
+    <h2>QuickCart</h2>
+    <button><span>Cart</span>🛒<span>({props.count})</span></button>
+      
+    </nav>
+    </>
+    
+  )
+}
 
